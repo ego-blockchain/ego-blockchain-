0f138ee741f8492ba15cc2ca9b6f22c84539f4b7
Artit <artitmuhaxhiri@hotmail.com>
2026-03-16 04:58:50 -0600

    feat: set Oracle Cloud node as default bootstrap peer

--- ego-desktop-mvp/src-frontend/config.ts
+++ ego-desktop-mvp/src-frontend/config.ts
@@ -1,2 +1,4 @@
-export const RELAY_HTTP = 'http://EgoRelay.egoblockchain.com:8080';
-export const RELAY_P2P  = '/dns4/EgoRelay.egoblockchain.com/tcp/4001/p2p/12D3KooWPj6m7jzmVyMh1zWrsoux3YiVs9j2HwsjrFXzDcqAGGz4';
+export const RELAY_HTTP      = 'http://EgoRelay.egoblockchain.com:8080';
+export const RELAY_P2P       = '/dns4/EgoRelay.egoblockchain.com/tcp/4001/p2p/12D3KooWPj6m7jzmVyMh1zWrsoux3YiVs9j2HwsjrFXzDcqAGGz4';
+export const RPC_URL         = 'https://rpc.egoblockchain.com';
+export const BOOTSTRAP_PEER  = '/dns4/rpc.egoblockchain.com/tcp/9000/p2p/12D3KooWMNRh7dJePAgtaZiwFCTisevVJQ5E52SpPqQqUPbHpJ72';
