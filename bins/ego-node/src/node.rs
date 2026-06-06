26d2111406ddc0a6c6194588c9005fc49f0f8b62
Artit <artitmuhaxhiri@hotmail.com>
2026-06-06 00:57:35 -0600

    feat: enable macOS code signing

--- bins/ego-node/src/node.rs
+++ bins/ego-node/src/node.rs
@@ -145,8 +145,8 @@ impl Node {
             .timeout(Duration::from_secs(30))
             .or_transport(relay_transport)
             .map(|either, _| match either {
-                libp2p::core::either::Either::Left((p, m)) => (p, libp2p::core::muxing::StreamMuxerBox::new(m)),
-                libp2p::core::either::Either::Right((p, m)) => (p, libp2p::core::muxing::StreamMuxerBox::new(m)),
+                libp2p::core::Either::Left((p, m)) => (p, libp2p::core::muxing::StreamMuxerBox::new(m)),
+                libp2p::core::Either::Right((p, m)) => (p, libp2p::core::muxing::StreamMuxerBox::new(m)),
             })
             .boxed();
 
